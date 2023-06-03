75042016199b3453b0edc97624ae987e073f765b
Bagoesardhia <96311137+Bagoesardhia@users.noreply.github.com>
2023-06-03 12:30:07 +0700

    03/06/2023

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("test 1");
+function validate(){
+    let x = document.forms['form-register']['first_name'].value
+    if (x === ''){
+        alert('nama harus diisi')
+        return false
+    }
+    alert('berhasil')
+    return true
+}
